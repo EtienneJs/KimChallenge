c5611d5adb9915cf73ec2492f2c2579de36e399c
eter19 <claudita.et@gmail.com>
2022-05-11 05:51:12 -0400

    primera subida al repositorio, si hizo la conexion a la api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+import { useState } from 'react';
+
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com',
+  cache: new InMemoryCache()
+});
+const LIST_COUNTRIES = gql`
+{
+  countries {
+    name
+    code
+    languages {
+      code
+      name
+    }
+    continent {
+      code 
+      name
+    }
+  }
+}
+`;
+
 
 function App() {
+  const [country, setCountry] = useState('US');
+  const {data, loading, error} = useQuery(LIST_COUNTRIES, {client});
+
+
+  if (loading || error) {
+    return <p>{error ? error.message : 'Loading...'}</p>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select value={country} onChange={event => setCountry(event.target.value)}>
+      {data.countries.map(country => (
+        <option key={country.code} value={country.code}>
+          { console.log(country.languages[0].name) }
+          {(loading || error) && error ? error.message : 'loading...' }
+          {country.name}
+        </option>
+      ))}
+    </select>
     </div>
   );
 }
